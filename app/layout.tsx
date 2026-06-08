62c2031ec02aeeb56f41a0928a830d8d83f5fc81
Natnael Getachew <natishget@gmail.com>
2026-06-08 08:44:44 +0300

    feat: enable Intro component in RootLayout and expand Prisma schema with new models and enums

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={` h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        {/* <Intro /> */}
+        <Intro />
         <Navbar />
         {children}
         <Footer />
