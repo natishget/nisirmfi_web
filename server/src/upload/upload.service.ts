55809800a34aad8c35ac1599bd861ff9c42b2770
Natnael Getachew <natishget@gmail.com>
2026-07-21 15:44:08 +0300

    fix: validate cloudinary upload result to avoid typescript errors

--- server/src/upload/upload.service.ts
+++ server/src/upload/upload.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import cloudinary from './config/cloudinary';
+import { Readable } from 'stream';
+import { UploadApiResponse } from 'cloudinary';
 
 @Injectable()
 export class UploadService {
-  async uploadToCloudinary(file: Express.Multer.File) {
-    try {
-      return await cloudinary.uploader.upload(file.path);
-    } catch (e: any) {
-      throw new Error(`Failed to upload file to Cloudinary: ${e.message}`);
-    }
+  async uploadToCloudinary(file: Express.Multer.File): Promise<UploadApiResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = cloudinary.uploader.upload_stream(
+        { folder: 'nisirmfi' },
+        (error, result) => {
+          if (error) return reject(new Error(`Failed to upload file to Cloudinary: ${error.message}`));
+          if (!result) return reject(new Error('Upload to Cloudinary failed: result is undefined'));
+          resolve(result);
+        }
+      );
+
+      const stream = new Readable();
+      stream.push(file.buffer);
+      stream.push(null);
+      stream.pipe(upload);
+    });
   }
 
   async deleteImage(publicId: string) {
